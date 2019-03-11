f241071669766206cd06031e5bbdc21896836dbb
Evan Mills <ejmills@iastate.edu>
2019-03-11 17:31:33 -0500

    Longest run

--- Debugging_Practice/src/Bad_Code/LoopExamples.java
+++ Debugging_Practice/src/Bad_Code/LoopExamples.java
@@ -24,6 +24,20 @@ public class LoopExamples {
 		return result;
 	}
 	
-	
+	public static int longestRun(String[] a) {
+		int count = 1;
+		int max = 1;
+		for(int i = 1; i < a.length; i++) {
+			if(a[i].equals(a[i--])) {
+				count ++;
+			}
+			else {
+				max = Math.max(count, max);
+				count = 1;
+			}
+		}
+		
+		return count;
+	}
 
 }
--- Debugging_Practice/src/Bad_Code/Main.java
+++ Debugging_Practice/src/Bad_Code/Main.java
@@ -2,10 +2,14 @@ package Bad_Code;
 
 public class Main {
 	public static void main(String[] args) {
-		String[] words = {"hello", "Hello", "Hi", "Goodbye"};
-		
-		LoopExamples le = new LoopExamples();
+		String[] words = {"hello", "Hi", "Goodbye", "Goodbye"};
+		String[] words2 = new String[4];
 		
 		System.out.println(LoopExamples.hasWord(words, "Goodbye"));
+		
+		System.out.println(LoopExamples.reverseString("Hello"));
+		
+		System.out.println(LoopExamples.longestRun(words));
+		
 	}
 }
