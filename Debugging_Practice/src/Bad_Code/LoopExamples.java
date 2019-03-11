11858f0bcfdfd29374d3f485e7e5163d12f64c48
Zoe Sanders <ZoeS@zsanders.student.iastate.edu>
2019-03-11 16:50:28 -0500

    Created first buggy method

--- Debugging_Practice/src/Bad_Code/LoopExamples.java
+++ Debugging_Practice/src/Bad_Code/LoopExamples.java
@@ -2,7 +2,17 @@ package Bad_Code;
 
 public class LoopExamples {
 	
-	public static 
+	public static boolean hasWord(String[] wordArray, String word)
+	{
+		for(int i = 0; i < wordArray.length; i++)
+		{
+			if(wordArray[i] == word)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
 	
 	
 
