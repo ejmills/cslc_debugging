4f023adc9dee653085de3526ef37d6a0bfd7511c
Evan Mills <ejmills@iastate.edu>
2019-03-11 16:58:06 -0500

    main stuff

--- Debugging_Practice/src/Bad_Code/Main.java
+++ Debugging_Practice/src/Bad_Code/Main.java
@@ -2,6 +2,10 @@ package Bad_Code;
 
 public class Main {
 	public static void main(String[] args) {
+		String[] words = {"hello", "Hello", "Hi", "Goodbye"};
 		
+		LoopExamples le = new LoopExamples();
+		
+		System.out.println(LoopExamples.hasWord(words, "Goodbye"));
 	}
 }
